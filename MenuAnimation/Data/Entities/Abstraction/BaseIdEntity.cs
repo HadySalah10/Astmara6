aaf508288999e0530a9a3cae7eea1538e5df2c2f
HadySalah10 <47247169+HadySalah10@users.noreply.github.com>
2019-04-30 14:40:26 +0200

    identity

--- MenuAnimation/Data/Entities/Abstraction/BaseIdEntity.cs
+++ MenuAnimation/Data/Entities/Abstraction/BaseIdEntity.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Data.Entities.Abstraction
@@ -8,6 +9,7 @@ namespace Data.Entities.Abstraction
     class BaseIdEntity
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
     }
 }
