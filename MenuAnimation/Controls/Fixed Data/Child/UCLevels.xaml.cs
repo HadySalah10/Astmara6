63dcd8f919f562e51744a549d5b30e27c2b341ef
HadySalah10 <47247169+HadySalah10@users.noreply.github.com>
2019-05-01 05:28:11 +0200

    finishing of UcLevels

--- MenuAnimation/Controls/Fixed Data/Child/UCLevels.xaml.cs
+++ MenuAnimation/Controls/Fixed Data/Child/UCLevels.xaml.cs
@@ -1,4 +1,13 @@
 using Astmara6Con.Controls;
+using Data.Context;
+using Data.Entities;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -10,11 +19,24 @@ namespace Astmara6Con
     /// </summary>
     public partial class UCLevels : UserControl
     {
+        public void loadData()
+        {
+            CollegeContext cd = new CollegeContext();
+
+            var levels = (from p in cd.Levels
+                          select p).ToList();
+
+            DGLevelsView.ItemsSource = levels;
+        }
+
+
         string STRNamePage;
         readonly FRMMainWindow Form = Application.Current.Windows[0] as FRMMainWindow;
         public UCLevels()
         {
             InitializeComponent();
+            loadData();
+
         }
 
         private void BTNBack_Click(object sender, RoutedEventArgs e)
@@ -31,6 +53,115 @@ namespace Astmara6Con
             Form.gridShow.Children.Add(new UCDepartment());
             STRNamePage = "الاقسام";
             Form.ChFormName(STRNamePage);
+        }
+
+        private void TBNameLevels_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+
+
+
+        private void BTNAdd_Click_1(object sender, RoutedEventArgs e)
+        {
+            //CollegeContext dataContext = new CollegeContext();
+            //Level LevelRow = DGLevelsView.SelectedItem as Level;
+
+            //Level levels = (from p in dataContext.Levels
+            //                where p.Id == 
+            //                select p).Single();
+
+
+            CollegeContext db = new CollegeContext();
+            db.Levels.Add(new Level()
+            {
+                Name = TBNameLevels.Text
+
+            });
+            db.SaveChanges();
+            loadData();
+            MessageBox.Show("تم حفظ العملية بنجاح");
+            TBNameLevels.Text = "";
+
+        }
+
+        private void BTNEdit_Click(object sender, RoutedEventArgs e)
+        {
+            
+            try
+            {
+                CollegeContext dataContext = new CollegeContext();
+                Level LevelRow = DGLevelsView.SelectedItem as Level;
+
+                Level levels = (from p in dataContext.Levels
+                                where p.Id == LevelRow.Id
+                                select p).Single();
+                levels.Name = LevelRow.Name;
+                dataContext.SaveChanges();
+                loadData();
+
+
+                MessageBox.Show("Row Updated Successfully.");
+
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show(Ex.Message);
+                return;
+            }
+            TBNameLevels.Text = "";
+
+        }
+
+        private void DGLevelsView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+
+        }
+
+        private void DGLevelsView_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BTNRemove_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                CollegeContext cd = new CollegeContext();
+                Level LevelRow = DGLevelsView.SelectedItem as Level;
+
+                Level levels = (from p in cd.Levels
+                                where p.Id == LevelRow.Id
+                                select p).Single();
+                cd.Levels.Remove(levels);
+                cd.SaveChanges();
+                loadData();
+
+                MessageBox.Show("Row Deleted Successfully.");
+            }
+            catch (Exception) { MessageBox.Show("حدث خطب ما برجاء المحاولة مرة أخري"); }
+
+        }
+
+        private void BTNRemoveAll_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                CollegeContext cd = new CollegeContext();
+
+                cd.Levels.RemoveRange(cd.Levels);
+
+                cd.SaveChanges();
+                loadData();
+
+                MessageBox.Show("All Data Are Deleted Successfully.");
+            }
+            catch (Exception) { MessageBox.Show("حدث خطب ما برجاء المحاولة مرة أخري"); }
+
+
         }
     }
 }
--- MenuAnimation/Properties/Settings.Designer.cs
+++ MenuAnimation/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Astmara6Con.Properties {
+namespace Astmara6.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
@@ -20,6 +20,16 @@ namespace Astmara6Con.Properties {
         public static Settings Default {
             get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.\\sqlexpress;Initial Catalog=Astmara6;Integrated Security=True")]
+        public string Astmara6ConnectionString {
+            get {
+                return ((string)(this["Astmara6ConnectionString"]));
             }
         }
     }
